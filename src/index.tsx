7651ea04c7eb991cf394b3dd2b34c76b1b036f0c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-11-01 14:06:38 -0500

    Fix CI

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
 
 ReactDOM.render(
   <React.StrictMode>
