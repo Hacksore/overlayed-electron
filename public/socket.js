253268f3932ff8d57f15f2d8d57aaa2fc19bc358
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-11-22 19:37:50 -0600

    More tweaks

--- public/socket.js
+++ public/socket.js
@@ -3,6 +3,14 @@ const RPC = require("@hacksore/discord-rpc");
 // The overlayed prod client id
 const CLIENT_ID = "905987126099836938";
 
+function uuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 // Events that we want to sub/unsub from
 const SUBSCRIBABLE_EVENTS = [
   "VOICE_STATE_CREATE",
@@ -22,20 +30,22 @@ class SocketManager {
     this.overlayed = overlayed;
     this.client = null;
 
-    this.connect();
+    this.client = new RPC.Client({ transport: "ipc" });
+
   }
 
-  connect() {
-    this.client = new RPC.Client({ transport: "ipc" });
-  }
 
   setupAuthAndListeners() {
     this.client.on("ready", async () => {
-      this.onReady();
+      // sub to voice channel changes
+      this.client.subscribe("VOICE_CHANNEL_SELECT");
+
+      // Get the users currently joined channel
+      this.client.send({ cmd: "GET_SELECTED_VOICE_CHANNEL", nonce: uuid() });
 
       // tell client we are ready
       this.sendElectronMessage({
-        event: "READY",
+        evt: "READY",
         data: {
           profile: this.client.user,
         },
@@ -57,71 +67,33 @@ class SocketManager {
     this.client.on("message", this.onDiscordMessage.bind(this));
   }
 
-  async onReady() {
-    // sub to voice channel changes
-    this.client.subscribe("VOICE_CHANNEL_SELECT");
-
-    // map all events back to onDiscordMessage method
-
-    // Get the users currently joined channel
-    this.client.transport.send({ cmd: "GET_SELECTED_VOICE_CHANNEL", nonce: "dead" });
-
-
-    // // called when the user joins/switches channels
-    // this.client.on("VOICE_CHANNEL_SELECT", event => {
-    //   // unsub from old channels first!
-    //   // TODO: figure out how to unsub from old channels
-    //   // if (this.overlayed.subscriptions) {
-    //   //   this.overlayed.subscriptions.map(async subscription => {
-    //   //     if (subscription) {
-    //   //       const item = await subscription;
-    //   //       item.unsubscribe();
-    //   //     }
-    //   //   });
-    //   // }
-
-    //   console.log("Switching channels", event);
-    //   // Get the users currently joined channel and send info
-    //   this.client.request("GET_CHANNEL", { channel_id: event.channel_id }).then(channelData => {
-    //     console.log(channelData.name, "id", event.channel_id);
-    //     this.sendElectronMessage({ event: "GET_CHANNEL", data: channelData });
-    //   });
-
-    //   this.overlayed.subscriptions = this.subscribeAllEvents(event.channel_id);
-    //   this.onDiscordMessage("VOICE_CHANNEL_SELECT", event);
-    // });
-
-    // // // Get the users currently joined channel
-    // this.client.request("GET_SELECTED_VOICE_CHANNEL").then(channelData => {
-    //   if (channelData.id) {
-    //     this.onDiscordMessage("GET_SELECTED_VOICE_CHANNEL", channelData);
-    //     this.overlayed.subscriptions = this.subscribeAllEvents(channelData.id);
-    //   }
-    // });
-  }
-
   /**
    * Subscribe to events by channelId defined in getRPCEvents
    * @returns {Array} - an array of subscribed events
    */
   subscribeAllEvents(channelId) {
-    const subscriptions = SUBSCRIBABLE_EVENTS.map(eventName => {
-      const sub = this.client.subscribe(eventName, { channel_id: channelId });
-      this.client.on(eventName, this.onDiscordMessage.bind(this, eventName));
-
-      return sub;
-    });
-
-    return subscriptions;
+    SUBSCRIBABLE_EVENTS.map(eventName =>
+      this.client.send({
+        cmd: "SUBSCRIBE",
+        args: { channel_id: channelId },
+        evt: eventName,
+        nonce: uuid(),
+      })
+    );
   }
 
   /**
-   * Map the events our local method
+   * Unsubscribe to events by channelId defined in getRPCEvents
    */
-  mapEventsToMessage() {
-    SUBSCRIBABLE_EVENTS.forEach(eventName => {
-      this.client.on(eventName, this.onDiscordMessage.bind(this, eventName));
-    });
+  unsubscribeAllEvents(channelId) {
+    SUBSCRIBABLE_EVENTS.map(eventName =>
+      this.client.send({
+        cmd: "UNSUBSCRIBE",
+        args: { channel_id: channelId },
+        evt: eventName,
+        nonce: uuid(),
+      })
+    );
   }
 
   /**
@@ -131,6 +103,8 @@ class SocketManager {
   onElectronMessage(message) {
     const { event, data } = JSON.parse(message);
     // console.log(event, data);
+
+    // TODO: add back events
   }
 
   /**
@@ -138,15 +112,27 @@ class SocketManager {
    * @param {string} message
    */
   onDiscordMessage(message) {
-    const { evt, data } = message;
-    // forward every packet from the socket to the client
-    console.log(message);
+    const { evt, cmd, data } = message;
 
     if (evt === "VOICE_CHANNEL_SELECT") {
-      // this.client.request("GET_SELECTED_VOICE_CHANNEL");
+      // attempt to unsub prior channel if found
+      if (this.overlayed.lastChannelId) {
+        this.unsubscribeAllEvents(data.channel_id);
+      }
+
+      this.client.send({ cmd: "GET_SELECTED_VOICE_CHANNEL", nonce: uuid() });
+
+      this.overlayed.lastChannelId = this.overlayed.curentChannelId;
+      this.overlayed.curentChannelId = data.channel_id;
     }
 
-    this.sendElectronMessage({ evt, data });
+    // sub to channel events
+    if (cmd === "GET_SELECTED_VOICE_CHANNEL") {
+      this.subscribeAllEvents(data.id);
+    }
+
+    // forward every packet from the socket to the client
+    this.sendElectronMessage(message);
   }
 
   /**
@@ -157,40 +143,6 @@ class SocketManager {
     this._win.webContents.send("fromMain", JSON.stringify(message));
   }
 
-  /**
-   * Send a message to discord socket
-   * @param {Object} message - the message to send in object format
-   */
-  sendDiscordMessage(message) {
-    this._socket.send(JSON.stringify(message));
-  }
-
-  /**
-   * Destroy he discord websocket
-   */
-  destroy() {
-    this._socket.close();
-  }
-
-  /**
-   * When the discord websocket errors
-   * @param {Error} event
-   */
-  onError(event) {}
-
-  /**
-   * When we connect to the discord RPC websocket
-   */
-  open() {
-    console.log("connected");
-  }
-
-  /**
-   * When we disconnect from the discord RPC websocket
-   */
-  close(err) {
-    console.log("disconnected", err);
-  }
 }
 
 module.exports = SocketManager;
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -28,7 +28,7 @@ class IPCSocketService extends EventEmitter {
   }
 
   init() {
-    this.send({ event: "I_AM_READY" });
+    this.send({ evt: "I_AM_READY" });
   }
 
   /**
@@ -45,58 +45,59 @@ class IPCSocketService extends EventEmitter {
    */
   onMessage(message: any) {
     const packet = JSON.parse(message);
-    const { event, data } = packet;
+    const { evt, cmd = null, data } = packet;
+    console.log(packet);
 
     // we get any ready data from main process
-    if (event === CustomEvents.READY) {
+    if (evt === CustomEvents.READY) {
       store.dispatch(setIsAuthed(true));
       store.dispatch(setReadyState(true));
       store.dispatch(setProfile(data.profile));
     }
 
     // electron did the work for us and got a token ;)
-    if (event === CustomEvents.CLICKTHROUGH_STATUS) {
+    if (evt === CustomEvents.CLICKTHROUGH_STATUS) {
       store.dispatch(setClickThrough(packet.value));
     }
 
     // custom pin status from main proc
-    if (event === CustomEvents.PINNED_STATUS) {
+    if (evt === CustomEvents.PINNED_STATUS) {
       store.dispatch(setPinned(packet.value));
     }
 
     // get a current channel info and list of voice states
-    if (event === RPCCommands.GET_SELECTED_VOICE_CHANNEL) {
+    if (cmd === RPCCommands.GET_SELECTED_VOICE_CHANNEL) {
       store.dispatch(setCurrentVoiceChannel(data));
       store.dispatch(setAppUsers(data.voice_states));
     }
 
     // get a list of the channel voice states
-    if (event === RPCCommands.GET_CHANNEL) {
+    if (evt === RPCCommands.GET_CHANNEL) {
       store.dispatch(setAppUsers(data.voice_states));
       store.dispatch(setCurrentVoiceChannel(data));
     }
 
     // start speaking
-    if (event === RPCEvents.SPEAKING_START) {
+    if (evt === RPCEvents.SPEAKING_START) {
       store.dispatch(setUserTalking({ id: data.user_id, value: true }));
     }
 
     // stop speaking
-    if (event === RPCEvents.SPEAKING_STOP) {
+    if (evt === RPCEvents.SPEAKING_STOP) {
       store.dispatch(setUserTalking({ id: data.user_id, value: false }));
     }
 
     // join
-    if (event === RPCEvents.VOICE_STATE_CREATE) {
+    if (evt === RPCEvents.VOICE_STATE_CREATE) {
       store.dispatch(addUser(data));
     }
 
     // leave
-    if (event === RPCEvents.VOICE_STATE_DELETE) {
+    if (evt === RPCEvents.VOICE_STATE_DELETE) {
       store.dispatch(removeUser(data.user.id));
     }
 
-    if (event === RPCEvents.VOICE_STATE_UPDATE) {
+    if (evt === RPCEvents.VOICE_STATE_UPDATE) {
       store.dispatch(updateUser(data));
     }
 
