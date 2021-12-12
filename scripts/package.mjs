01a2dfd0663c211538495fd1cc45ee038c866325
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-12-12 10:43:03 -0600

    Test publish

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -6,9 +6,13 @@ import chalk from "chalk";
 
 const TAG = chalk.bgBlue("[build.mjs]");
 
+// grab the env to see if we should publish
+const { GIT_BRANCH = "refs/heads/master" } = process.env;
+const publish = GIT_BRANCH === "refs/heads/master" ? "always" : "never";
+
 async function packElectron() {
   return electronBuild({
-    publish: "always",
+    publish,
     config: builderConfig,
   }).then(result => {
     console.log(TAG, "files:", chalk.green(result));
