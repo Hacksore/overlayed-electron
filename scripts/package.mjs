bf3dc4d43cca51571ba4880a8a16f5d26b4994fe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-12-12 10:00:11 -0600

    CI/CD updates

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -4,10 +4,13 @@ import { build as electronBuild } from "electron-builder";
 import { config as builderConfig } from "../configs/electron-builder.config.mjs";
 import chalk from "chalk";
 
+const { PUBLISH = "never" } = process.env;
+
 const TAG = chalk.bgBlue("[build.mjs]");
 
 async function packElectron() {
   return electronBuild({
+    publish: PUBLISH,
     config: builderConfig,
   }).then(result => {
     console.log(TAG, "files:", chalk.green(result));
