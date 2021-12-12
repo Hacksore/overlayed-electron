c6299a2476be8b34fea237ada22d6d9c4641afe8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-12-12 15:06:35 -0600

    Put back env var

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -6,15 +6,15 @@ import chalk from "chalk";
 
 const TAG = chalk.bgBlue("[build.mjs]");
 
-const { GIT_BRANCH = "refs/heads/master" } = process.env;
+const { GIT_BRANCH = "master" } = process.env;
 
 // only attempt to publish changes on a master build
-const publish = GIT_BRANCH === "refs/heads/master" ? "always" : "never";
+const publish = GIT_BRANCH === "master" ? "always" : "never";
 
 console.log("Current branch", GIT_BRANCH);
 
 // only notarize in master branch
-if (GIT_BRANCH !== "refs/heads/master") {
+if (GIT_BRANCH !== "master") {
   delete process.env.APPLE_ID;
   delete process.env.APPLE_PASSWORD;
 }
